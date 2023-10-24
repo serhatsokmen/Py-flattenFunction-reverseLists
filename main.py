bc69a6d6f2c1cf3d96dceb4905057fdb16187f04
Serhat <135127450+serhatsokmen@users.noreply.github.com>
2023-10-25 00:02:14 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+from collections.abc import Iterable
+
+"""
+1- Bir listeyi düzleştiren (flatten) fonksiyon yazın. Elemanları birden çok katmanlı listelerden ([[3],2] gibi) oluşabileceği gibi, non-scalar verilerden de oluşabilir. Örnek olarak:
+
+input: [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
+
+output: [1,'a','cat',2,3,'dog',4,5]
+"""
+
+
+lst = [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
+
+
+def flattenFunction(nList):
+    flattenList = []
+    for i in nList:
+        if type(i) == list:
+            flattenList += flattenFunction(i)
+        else:
+            flattenList.append(i)
+
+
+    return flattenList
+
+print(flattenFunction(lst))
+
+
+
+"""
+2- Verilen listenin içindeki elemanları tersine döndüren bir fonksiyon yazın. Eğer listenin içindeki elemanlar da liste içeriyorsa onların elemanlarını da tersine döndürün. Örnek olarak:
+
+input: [[1, 2], [3, 4], [5, 6, 7]]
+
+output: [[[7, 6, 5], [4, 3], [2, 1]]
+"""
+
+lst2 = [[1,2],[3,4],[5,6,7]]
+
+
+def reversePrint(lists):
+    lists = lists[::-1]
+    lists = [i[::-1] for i in lists]
+    return lists
+
+print(reversePrint(lst2))
+
+
+
